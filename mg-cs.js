e5639ea5b7d0681ca7d8df0c79eeeb27dad8f6dd
Tyler Long <tyler4long@gmail.com>
2016-06-28 16:02:02 +0800

    Read json

--- mg-cs.js
+++ mg-cs.js
@@ -6,8 +6,35 @@ program
   .parse(process.argv);
 
 if(program.model === undefined) {
-  console.log('options required: -m <model>');
+  console.error('options required: -m <model>');
   return;
 }
 
 console.log('generate model for: ' + program.model);
+
+
+var fs = require('fs');
+var path = require('path');
+var deepmerge = require('deepmerge');
+
+
+var folder = './json/' + program.model + '/get/';
+fs.access(folder, fs.F_OK, function(err) {
+  if(err) {
+    console.error("Folder " + folder + " doesn't exist!");
+  } else {
+    var json = read_json_folder(folder);
+    console.log(json);
+  }
+});
+
+var read_json_folder = (folder) => {
+  return fs.readdirSync(folder)
+    .filter((file) =>  path.extname(file) === '.json')
+    .map((file) => read_json_file(path.join(folder, file)))
+    .reduce((prev, data) => deepmerge(prev, data), {});
+}
+
+var read_json_file = (file) => {
+  return JSON.parse(fs.readFileSync(file, 'utf8'));
+}
