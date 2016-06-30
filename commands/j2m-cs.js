5c0807f14313f2223e756851f2cb95cc650045db
Tyler Long <tyler4long@gmail.com>
2016-06-30 22:30:16 +0800

    Get commander arguments right

--- commands/j2m-cs.js
+++ commands/j2m-cs.js
@@ -2,14 +2,14 @@ const program = require('commander');
 
 program
   .version(require('../package.json').version)
-  .option('-j, --json <file>', 'json file')
-  .option('-d, --dir <folder>', 'directory which contains json files')
-  .option('-m, --model <model>', 'model name')
+  .option('-j, --json <json>', 'path to json file')
+  .option('-f, --folder <folder>', 'directory which contains json files')
+  .option('-m, --model <model>', 'the generated model name')
   .parse(process.argv);
 
 
 const fs = require('fs');
-const json = require('../json');
+const jsonUtil = require('../json');
 const inspector = require('../inspector');
 const nunjucks = require('../nunjucks');
 
@@ -20,7 +20,7 @@ fs.access(folder, fs.F_OK, (err) => {
   if (err) {
     console.error(`Folder ${folder} doesn't exist!`);
   } else {
-    const data = json.read_folder(folder);
+    const data = jsonUtil.read_folder(folder);
     const cls = inspector.get_class(program.model, data);
     let template = nunjucks.render('csharp/csharp.cs', { cls });
     template = template.replace(/ +$/gm, '');
