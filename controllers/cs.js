b2df50ff4b3c369513de224d0931a3d8ecc90526
Tyler Long <tyler4long@gmail.com>
2016-07-01 16:05:57 +0800

    Refactor code

--- controllers/cs.js
+++ controllers/cs.js
@@ -1,23 +1,15 @@
-const fs = require('fs');
-const JsonHelper = require('../helpers/json');
 const inspector = require('../inspector');
 const nunjucks = require('../nunjucks');
 
 
-const process = (program) => {
-  const folder = program.folder;
-  fs.access(folder, fs.F_OK, (err) => {
-    if (err) {
-      console.error(`Folder ${folder} doesn't exist!`);
-    } else {
-      const data = JsonHelper.readFolder(folder);
-      const cls = inspector.get_class(program.model, data);
-      let template = nunjucks.render('csharp/csharp.cs', { cls });
-      template = template.replace(/ +$/gm, ''); // trim blank lines
-      console.log(template);
-    }
-  });
+const CSharpController = {
+  render: (name, json) => {
+    const cls = inspector.get_class(name, json);
+    let model = nunjucks.render('csharp/csharp.cs', { cls });
+    model = model.replace(/ +$/gm, ''); // trim blank lines
+    return model;
+  },
 };
 
 
-module.exports = process;
+module.exports = CSharpController;
--- j2m.js
+++ j2m.js
@@ -1,14 +1,48 @@
 #! /usr/bin/env node
-
 const program = require('commander');
+const JsonHelper = require('./helpers/json');
 
+
+const options = new Map([
+  ['lang', ['-l, --lang <lang>',
+    'destination language file extension, such as: cs, js, java, php...etc.']],
+  ['json', ['-j, --json <json>', 'path to json file']],
+  ['folder', ['-f, --folder <folder>', 'folder which contains json files']],
+  ['name', ['-n, --name <name>',
+    'the generated model name, such as: user, account, film, course...etc.']],
+]);
 program
   .version(require('./package.json').version)
-  .option('-l, --lang <lang>', 'programming language file extension')
-  .option('-j, --json <json>', 'path to json file')
-  .option('-f, --folder <folder>', 'directory which contains json files')
-  .option('-m, --model <model>', 'the generated model name')
+  .option.apply(program, options.get('lang'))
+  .option.apply(program, options.get('json'))
+  .option.apply(program, options.get('folder'))
+  .option.apply(program, options.get('name'))
   .parse(process.argv);
 
+if (program.lang === undefined) {
+  console.error(`Please specify "${options.get('lang')[0]}" : ${options.get('lang')[1]}`);
+  process.exit(1);
+}
+if (typeof program.name !== 'string') {
+  console.error(`Please specify "${options.get('name')[0]}" : ${options.get('name')[1]}`);
+  process.exit(1);
+}
+if ((program.json === undefined && program.folder === undefined)
+  || (program.json !== undefined && program.folder !== undefined)) {
+  console.error(`Please specify either "${options.get('json')[0]}" \
+or "${options.get('folder')[0]}" but not both.`);
+  process.exit(1);
+}
 
-require(`./controllers/${program.lang}.js`)(program);
+let json = null;
+try {
+  json = program.json !== undefined ?
+    JsonHelper.readFile(program.json) : JsonHelper.readFolder(program.folder);
+} catch (e) {
+  console.error(e.message);
+  process.exit(1);
+}
+
+const LanguageController = require(`./controllers/${program.lang}.js`);
+const model = LanguageController.render(program.name, json);
+console.log(model);
