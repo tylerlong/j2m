4bc7eab42e8beb0331d22889230feeff0e8037a2
Tyler Long <tyler4long@gmail.com>
2016-07-01 11:06:50 +0800

    Add controllers folder

---
+++ controllers/cs.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const jsonUtil = require('../jsonUtil');
+const inspector = require('../inspector');
+const nunjucks = require('../nunjucks');
+
+
+const process = (program) => {
+  const folder = program.folder;
+  console.log(folder);
+  fs.access(folder, fs.F_OK, (err) => {
+    if (err) {
+      console.error(`Folder ${folder} doesn't exist!`);
+    } else {
+      const data = jsonUtil.read_folder(folder);
+      const cls = inspector.get_class(program.model, data);
+      let template = nunjucks.render('csharp/csharp.cs', { cls });
+      template = template.replace(/ +$/gm, '');
+      console.log(template);
+    }
+  });
+};
+
+
+module.exports = process;
--- j2m.js
+++ j2m.js
@@ -11,22 +11,4 @@ program
   .parse(process.argv);
 
 
-const fs = require('fs');
-const jsonUtil = require('./jsonUtil');
-const inspector = require('./inspector');
-const nunjucks = require('./nunjucks');
-
-
-const folder = program.folder;
-console.log(folder);
-fs.access(folder, fs.F_OK, (err) => {
-  if (err) {
-    console.error(`Folder ${folder} doesn't exist!`);
-  } else {
-    const data = jsonUtil.read_folder(folder);
-    const cls = inspector.get_class(program.model, data);
-    let template = nunjucks.render('csharp/csharp.cs', { cls });
-    template = template.replace(/ +$/gm, '');
-    console.log(template);
-  }
-});
+require(`./controllers/${program.lang}.js`)(program);
