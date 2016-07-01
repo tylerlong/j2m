c8fa5c3780d61635d31f5b85a55628624a0bbc68
Tyler Long <tyler4long@gmail.com>
2016-07-01 20:47:33 +0800

    Finish fixtures test case

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,36 +1,37 @@
-// const assert = require('assert');
+const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
+const JsonHelper = require('../helpers/json');
 
 
 const listFolders = (folder) => fs.readdirSync(folder).map((file) => path.join(folder, file))
   .filter((file) => fs.lstatSync(file).isDirectory());
 const listFiles = (folder) => fs.readdirSync(folder).map((file) => path.join(folder, file))
-  .filter((file) => fs.lstatSync(file).isFile()).map((file) => path.basename(file));
+  .filter((file) => fs.lstatSync(file).isFile())
+  .map((file) => path.basename(file));
 
-const jsonFolder = 'test/fixtures/json';
-const models = new Map(listFolders(jsonFolder).map((folder) => [path.basename(folder), listFiles(folder)]));
-
-console.log(models); // eslint-disable-line no-console
-// console.log(listFolders(jsonFolder)); // eslint-disable-line no-console
-// const listJsonFiles = (folder) => fs.readdirSync(folder)
-//   .filter((file) => path.extname(file) === '.json');
-
-// const modelFolders = fs.readdirSync(jsonFolder).map((file) => path.join(jsonFolder, file))
-//   .filter((file) => fs.lstatSync(file).isDirectory());
-// const models = new Map(modelFolders.map((folder) =>
-//   [path.basename(folder), listJsonFiles(folder)]));
-// console.log(models); // eslint-disable-line no-console
-//
-//
-// describe('fixtures', () => {
-//   fs.readdirSync(jsonFolder).map((file) => path.join(jsonFolder, file))
-//     .filter((file) => fs.lstatSync(file).isDirectory())
-//     .forEach((folder) => {
-//       describe(path.basename(folder), () => {
-//         it('should convert', () => {
-//
-//         });
-//       });
-//     });
-// });
+describe('fixtures', () => {
+  const languages = listFolders('test/fixtures')
+    .filter((folder) => path.basename(folder) !== 'json');
+  languages.forEach((language) => {
+    // eslint-disable-next-line global-require
+    const LanguageController = require(path.join('../controllers', path.basename(language)));
+    describe(path.basename(language), () => {
+      const models = listFolders(language);
+      models.forEach((model) => {
+        describe(path.basename(model), () => {
+          const samples = listFiles(model);
+          samples.forEach((sample) => {
+            it(`should generate ${sample} correctly`, () => {
+              const jsonFile = path.join('test/fixtures/json', path.basename(model),
+                sample.replace(/\.[^/.]+$/, '.json'));
+              const jsonData = JsonHelper.readFile(jsonFile);
+              const languageData = LanguageController.render(path.basename(model), jsonData);
+              assert.equal(fs.readFileSync(path.join(model, sample), 'utf8'), languageData);
+            });
+          });
+        });
+      });
+    });
+  });
+});
