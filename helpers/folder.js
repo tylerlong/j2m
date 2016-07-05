7b55d3c5faef18f8fab8323a085d31ecabcd64aa
Tyler Long <tyler4long@gmail.com>
2016-07-05 12:20:14 +0800

    Refactor code

--- helpers/folder.js
+++ helpers/folder.js
@@ -3,15 +3,16 @@ const path = require('path');
 
 
 // todo: use currying to refactor, check the idea of Ramda
-const FolderHelper = {
-  folderFilter: (folder, filter) => fs.readdirSync(folder)
-    .filter((file) => filter(path.join(folder, file)))
-    .map((file) => [file, path.join(folder, file)]),
-  listFolders: (folder) => FolderHelper.folderFilter(folder,
-    (file) => fs.lstatSync(file).isDirectory()),
-  listFiles: (folder) => FolderHelper.folderFilter(folder,
-    (file) => fs.lstatSync(file).isFile()),
-};
+
+const folderFilter = (folder, filter) => fs.readdirSync(folder)
+  .filter((file) => filter(path.join(folder, file)))
+  .map((file) => [file, path.join(folder, file)]);
+
+const listFolders = (folder) => folderFilter(folder,
+  (file) => fs.lstatSync(file).isDirectory());
+
+const listFiles = (folder) => folderFilter(folder,
+  (file) => fs.lstatSync(file).isFile());
 
 
-module.exports = FolderHelper;
+module.exports = { listFolders, listFiles };
