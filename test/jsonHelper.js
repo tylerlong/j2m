2cf1d7caae6f86abd50306e41ab53f83ba2c29ab
Tyler Long <tyler4long@gmail.com>
2016-07-05 15:14:46 +0800

    Finish testing for jsonHelper

--- test/jsonHelper.js
+++ test/jsonHelper.js
@@ -5,20 +5,21 @@ const jsonHelper = require('../helpers/json');
 describe('jsonHelper', () => {
   describe('readFiles()', () => {
     it('should read files', () => {
-      const json = jsonHelper.readFiles(['fixtures/json/test/1.json', 'fixtures/json/test/2.json']);
+      const json = jsonHelper.readFiles(['test/fixtures/json/test/1.json',
+        'test/fixtures/json/test/2.json']);
       assert.equal(`{
-"name": "Tyler Long",
-"age": 30
+    "name": "Tyler Long",
+    "age": 30
 }`, JSON.stringify(json, null, 4));
-    };
-  };
+    });
+  });
   describe('readFolder()', () => {
     it('should read folder', () => {
-      const json = jsonHelper.readFolder('fixtures/json/test/');
+      const json = jsonHelper.readFolder('test/fixtures/json/test/');
       assert.equal(`{
-"name": "Tyler Long",
-"age": 30
+    "name": "Tyler Long",
+    "age": 30
 }`, JSON.stringify(json, null, 4));
-    };
-  };
-};
+    });
+  });
+});
